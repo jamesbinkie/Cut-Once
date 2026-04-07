840a8cf744b744f46782d50d77c12e8aedeb463c
jamesbinkie <32223566+jamesbinkie@users.noreply.github.com>
2026-04-08 00:45:31 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mytestsite.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cut_once.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
