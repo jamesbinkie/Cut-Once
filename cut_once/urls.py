f0b74a52a6fbe6a3b6069afc0e24ec3c1984ab33
jamesbinkie <32223566+jamesbinkie@users.noreply.github.com>
2026-04-08 16:56:59 +0100

    Update urls.py

--- cut_once/urls.py
+++ cut_once/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # Add 'include' here
 from knowledge import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Fixed the '手' typo here
+    path('admin/', admin.site.urls),
     path('', views.home_search, name='home'),
+    
+    # This line adds login, logout, password resets, etc.
+    path('accounts/', include('django.contrib.auth.urls')), 
 ]
