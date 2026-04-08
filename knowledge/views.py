b9ebf963f1986a3b3f6de688d3a01675f063b947
jamesbinkie <32223566+jamesbinkie@users.noreply.github.com>
2026-04-08 16:50:39 +0100

    Update views.py

--- knowledge/views.py
+++ knowledge/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 @login_required
-def home(request):
+def home_search(request):
     return render(request, 'knowledge/search.html')
