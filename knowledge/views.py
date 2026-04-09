0d4f879b9cc78a5dc8565dbb679b7c6fd2b88717
jamesbinkie <32223566+jamesbinkie@users.noreply.github.com>
2026-04-09 11:39:27 +0100

    Update views.py

--- knowledge/views.py
+++ knowledge/views.py
@@ -1,7 +1,14 @@
+from django.shortcuts import render  # <--- This is the missing line
+from django.contrib.auth.decorators import login_required
+from .models import Article
+
+@login_required
 def home_search(request):
     query = request.GET.get('q')
     articles = []
+    
     if query:
+        # This searches for your query in the content of your articles
         articles = Article.objects.filter(content__icontains=query)
     
     return render(request, 'knowledge/search.html', {
