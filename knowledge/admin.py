3bafb606ddd57e5a77ae462671c313650db220eb
jamesbinkie <32223566+jamesbinkie@users.noreply.github.com>
2026-04-09 14:48:58 +0100

    Update admin.py

--- knowledge/admin.py
+++ knowledge/admin.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import Article
 
+from django.contrib import admin
+from .models import Article
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'owner', 'last_reviewed', 'review_status')
-    prepopulated_fields = {"slug": ("title",)} # Automatically fills the slug as you type the title
+    list_display = ('title', 'owner', 'last_reviewed')
+    prepopulated_fields = {"slug": ("title",)}
+
+    class Media:
+        # We will create this JS file next to handle the "Add/Remove" buttons
+        js = ('knowledge/js/page_builder.js',)
+        css = {
+            'all': ('knowledge/css/admin_builder.css',)
+        }
     
     def review_status(self, obj):
         if obj.needs_review:
