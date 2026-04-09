fad47ed3ae185115987acdb021f5fd89040af282
jamesbinkie <32223566+jamesbinkie@users.noreply.github.com>
2026-04-09 14:48:04 +0100

    Update models.py

--- knowledge/models.py
+++ knowledge/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from datetime import timedelta
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
@@ -9,6 +10,11 @@ class Article(models.Model):
     
     owner = models.CharField(max_length=100)
     last_reviewed = models.DateField(default=timezone.now)
-
+    
     def __str__(self):
         return self.title
+
+    @property
+    def needs_review(self):
+        # Returns True if the article hasn't been reviewed in 180 days
+        return timezone.now().date() > self.last_reviewed + timedelta(days=180)
