e36f01f7e9054cdb8f89b4e612c971486f00275b
jamesbinkie <32223566+jamesbinkie@users.noreply.github.com>
2026-04-09 15:11:04 +0100

    Update models.py

--- knowledge/models.py
+++ knowledge/models.py
@@ -1,20 +1,14 @@
 from django.db import models
 from django.utils import timezone
-from datetime import timedelta
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    # This stores our list of widgets: [{'type': 'text', 'value': '...'}, {'type': 'html', 'value': '...'}]
-    content_blocks = models.JSONField(default=list, blank=True) 
+    # Storing everything as a single HTML string for the Google Docs feel
+    content = models.TextField(blank=True) 
     
     owner = models.CharField(max_length=100)
     last_reviewed = models.DateField(default=timezone.now)
-    
+
     def __str__(self):
         return self.title
-
-    @property
-    def needs_review(self):
-        # Returns True if the article hasn't been reviewed in 180 days
-        return timezone.now().date() > self.last_reviewed + timedelta(days=180)
