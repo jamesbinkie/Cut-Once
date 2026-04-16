d5db1558da5afd503712a8d8182b1a352db053dc
jamesbinkie <32223566+jamesbinkie@users.noreply.github.com>
2026-04-16 11:31:14 +0100

    Update admin.py

--- knowledge/admin.py
+++ knowledge/admin.py
@@ -1,35 +1,54 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
 from django.utils.html import format_html
 from .models import Article
 
-# --- PROTECT SUPER ADMINS ---
+# --- 1. HIERARCHY & UI CLEANUP ---
 class CustomUserAdmin(UserAdmin):
+    # This removes the "User Permissions" box so you only see "Groups"
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups", # We keep Groups but hide individual perms
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        # If the logged-in person is NOT a Super-Admin, hide all Super-Admins from the list
+        # Tier 1 Protection: Hide Super-Admins from everyone except other Super-Admins
         if not request.user.is_superuser:
             return qs.filter(is_superuser=False)
         return qs
 
     def has_change_permission(self, request, obj=None):
-        # Prevent non-superusers from editing superusers
+        # Tier 1 Protection: Admins cannot edit Super-Admins
         if obj and obj.is_superuser and not request.user.is_superuser:
             return False
         return super().has_change_permission(request, obj)
 
     def has_delete_permission(self, request, obj=None):
-        # Only Super-Admins can delete other Super-Admins
+        # Tier 1 Protection: Admins cannot delete Super-Admins
         if obj and obj.is_superuser and not request.user.is_superuser:
             return False
         return super().has_delete_permission(request, obj)
 
-# Re-register User with our new protected rules
+# Re-register the User model with our clean UI and protection logic
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 
-# --- ARTICLE ADMIN (Your existing code + CKEditor) ---
+
+# --- 2. ARTICLE ADMIN (CKEditor Enabled) ---
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'owner', 'last_reviewed', 'review_status')
