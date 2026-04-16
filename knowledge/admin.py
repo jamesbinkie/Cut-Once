f248e5b7009f36922e7c53ebe0e6e00fbb0ae8df
jamesbinkie <32223566+jamesbinkie@users.noreply.github.com>
2026-04-16 12:10:56 +0100

    Update admin.py

--- knowledge/admin.py
+++ knowledge/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.utils.html import format_html
-from .models import Article
+from .models import Article, SearchHistory 
 
 # --- 1. HIERARCHY & AUTOMATION ---
 class CustomUserAdmin(UserAdmin):
